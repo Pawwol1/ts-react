687cba9896b6f3c86b60db8673cb550937130d30
Wolski Paweł <paws@jcommerce.pl>
2022-07-04 14:43:30 +0200

    header demo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,14 @@
 import { Button } from '@mui/material';
+import './header.css';
 import React from 'react'
 
 function Header() {
   return (
     <header>
-        
+      <div className="header_box">
+        <h1 className="header_title">Meet ReqRes users!</h1>
+        <p className="header_text">Application created with free ReqRes API</p>
+      </div>
     </header>
   )
 }
