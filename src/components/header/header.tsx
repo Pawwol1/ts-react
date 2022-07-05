682ddef72f6ad85d9308b847d82eed6af140b761
Wolski Paweł <paws@jcommerce.pl>
2022-07-05 10:53:45 +0200

    header completed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,16 @@
-import { Button } from '@mui/material';
-import './header.css';
 import React from 'react'
+import './header.css';
 
 function Header() {
+
+const title: string = "Meet ReqRes users"
+const text: string = "Application created with free ReqRes API"
+
   return (
     <header>
       <div className="header_box">
-        <h1 className="header_title">Meet ReqRes users!</h1>
-        <p className="header_text">Application created with free ReqRes API</p>
+        <h1 className="header_box--title">{title}</h1>
+        <p className="header_box--text">{text}</p>
       </div>
     </header>
   )
