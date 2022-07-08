9b3e5206cd6759ace6b90c5f5d33ef4e0cc77ddc
Wolski Paweł <paws@jcommerce.pl>
2022-07-08 13:13:49 +0200

    basic layout for userdetails created

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,12 @@ import PageNotFound from './components/pageNotFound/pageNotFound';
 import Sidebar from './components/sidebar/sidebar';
 import UserDetails from './components/userDetails/userDetails';
 import Userlist from './components/userlist/userlist';
-import UserlistSearch from './components/userlistSearch/userlistSearch';
-
 
 function App() {
   return (
     <>
       <Header/>
       <Userlist/>
-      {/* <UserlistSearch/> */}
       <UserDetails/>
       <PageNotFound/>
       <Sidebar/>
--- src/components/userDetails/userDetails.tsx
+++ src/components/userDetails/userDetails.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import "./userDetails.css";
 
 function UserDetails() {
     return (
-        <div>
-            
+        <div className='userDetails_box'>
+            <div className='userDetails_box--user'>
+                <h2>Jan Kowalski</h2>
+                <p>Miejsce na avatar</p>
+            </div>
+            <form className='userDetails_box--form' autoComplete='off'>
+                <h3 className="userDetails_box--form--contact">Contact with "Jan Kowalski"!</h3>
+                <label htmlFor="title" className="userDetails_box--form--title">
+                    Tytuł: 
+                    <input type="text" id="title" />
+                </label>
+                <label htmlFor="message" className="userDetails_box--form--message">
+                    <textarea name="message" id="message" cols={30} rows={10} placeholder="Zacznij pisać...">
+                    </textarea>
+                </label>
+                <button type="submit">Wyślij</button>
+            </form>
         </div>
     );
 }
