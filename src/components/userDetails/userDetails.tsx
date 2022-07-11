7c0d5823fc30df79b6f60114403c625f0278726d
Wolski Paweł <paws@jcommerce.pl>
2022-07-11 14:59:42 +0200

    form validation complete

--- src/components/userDetails/userDetails.tsx
+++ src/components/userDetails/userDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { User } from '../userlist/userlist';
 import "./userDetails.css";
@@ -13,6 +13,14 @@ function UserDetails() {
         avatar: ""
     });
     const [totalUsers, setTotalUsers] = useState<number>(1);
+    const [formMsg, setFormMsg] = useState({
+        toUser: "",
+        title: "",
+        message: ""
+    });
+    const [formTitleError, setFormTitleError] = useState<boolean>(false);
+    const [formMsgError, setFormMsgError] = useState<boolean>(false);
+
     const {userID} = useParams();
 
     useEffect(() => {
@@ -26,20 +34,61 @@ function UserDetails() {
             setUser(data.data);
         }
         getSingleUser();
-    }, [userID])
+    }, [userID]);
 
     useEffect(() => {
         const getTotalUsers = async () => {
             const resp = await fetch("https://reqres.in/api/users");
             if (!resp.ok) {
-                const err = "User not Found";
+                const err = "Total users not Found";
                 throw new Error(err);
             }
             const data = await resp.json();
             setTotalUsers(data.total);
         };
         getTotalUsers();
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        if (formMsg.title || formMsg.message !== "") {
+            setFormTitleError(formMsg.title.length < 2);
+            setFormMsgError(formMsg.message.length <= 4);
+        }
+    }, [formMsg])
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        setFormMsg({
+            toUser: "",
+            title: "",
+            message: ""
+        });
+
+        if (!formTitleError && !formMsgError && formMsg.title !== "" && formMsg.message !== "") {
+            console.log(formMsg);
+            };
+    }
+
+    const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setFormMsg(prev => {
+            return {
+                ...prev,
+                toUser: `${user?.first_name} ${user?.last_name}`,
+                title: e.target.value
+            }
+        });
+    }
+
+    const handleMsgChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setFormMsg(prev => {
+            return {
+                ...prev,
+                message: e.target.value
+            }
+        });
+        
+    }
 
     const userNotFound: string = "User not found";
     const contactUser: string = `Contact with ${user?.first_name} ${user?.last_name}!`;
@@ -47,6 +96,8 @@ function UserDetails() {
     const titleLabel: string = "Title: ";
     const textareaPlaceholder: string = "Start typing...";
     const btnMsg: string = "Send message";
+    const titleErr : string = "Please add a title";
+    const msgErr : string = "Message have to be longer than four letters";
 
     return (
         <>
@@ -55,20 +106,21 @@ function UserDetails() {
 
         ? <div className='userDetails_box'>
             <div className='userDetails_box--user'>
-                <h2>{user?.first_name} {user?.last_name}</h2>
+                <h3>{user?.first_name} {user?.last_name}</h3>
                 <img src={user?.avatar} alt="user avatar" />  
             </div>
-            <form className='userDetails_box--form' autoComplete='off'>
-                <h3 className="userDetails_box--form--contact">{contactUser}</h3>
+            <form className='userDetails_box--form' onSubmit={handleSubmit} autoComplete='off'>
+                <h4 className="userDetails_box--form--contact">{contactUser}</h4>
                 <label htmlFor="title" className="userDetails_box--form--title">
                     {titleLabel} 
-                    <input type="text" id="title" />
+                    <input type="text" id="title" value={formMsg.title} onChange={handleTitleChange}/>
                 </label>
+                {formTitleError && <p className="userDetails_box--form--error">{titleErr}</p>}
                 <label htmlFor="message" className="userDetails_box--form--message">
-                    <textarea name="message" id="message" cols={30} rows={10} placeholder={textareaPlaceholder}>
-                    </textarea>
+                    <textarea name="message" id="message" value={formMsg.message} onChange={handleMsgChange} cols={25} rows={10} placeholder={textareaPlaceholder}/>
                 </label>
-                <button type="submit">{btnMsg}</button>
+                {formMsgError && <p className="userDetails_box--form--error">{msgErr}</p>}
+                <button className='userDetails_box--form--button' type="submit" disabled={formTitleError || formMsgError}>{btnMsg}</button>
             </form>
           </div>
 
