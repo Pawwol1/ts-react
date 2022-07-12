1ae609909aa72efa8feb486cc4e17835312d2e6f
Wolski Paweł <paws@jcommerce.pl>
2022-07-12 12:15:55 +0200

    sidebar with drawer functionality complete

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,10 +1,19 @@
 import React, {useEffect, useState} from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
 import './sidebar.css';
+
+type Anchor = 'left' | 'right';
 
 function Sidebar() {
 
     const [shiba, setShiba] = useState<string>("");
     const [refresh, setRefresh] = useState<number>(1);
+    const [isOpen, setIsOpen] = useState({
+        left: false,
+        right: false,
+      });
 
     useEffect(() => {
         const getShibaImage = async () => {
@@ -23,17 +32,61 @@ function Sidebar() {
         setRefresh(Math.random());
     }
 
-    const sidebarTitle: string = "Give a smile to shiba!";
-    const sidebarText: string = "We prepared cute shiba images to make your day better!";
-    const btnText: string = "Get new shiba image";
+    const toggleSidebar = (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
 
-    return (
+      setIsOpen({ ...isOpen, [anchor]: open });
+    };
+
+  const contentBox = (anchor: Anchor) => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      
+      onKeyDown={toggleSidebar(anchor, false)}
+    >
         <div className='sidebar_box'>
+            <div className='sidebar_box--close'>
+              <p onClick={toggleSidebar(anchor, false)}>X</p>
+            </div>
             <h2 className='sidebar_box--title'>{sidebarTitle}</h2>
             <p className='sidebar_box--text'>{sidebarText}</p>
             <img className='sidebar_box--img' src={shiba} alt="shiba image" />
             <button className='sidebar_box--button' onClick={handleClick}>{btnText}</button>
         </div>
+    </Box>
+  );
+
+    const sidebarTitle: string = "Give a smile to shiba!";
+    const sidebarText: string = "We prepared cute shiba images to make your day better!";
+    const btnText: string = "Get new shiba image";
+    const boxTitle: string = "Please choose which side you want to see the surprise on:";
+
+    return (
+      <>
+        <h3 style={{padding: "3rem 0 0 0", textAlign: "center"}}>{boxTitle}</h3>
+          <div className="sidebar_open">
+            {(['left', 'right'] as const).map((anchor) => (
+            <div key={anchor}>
+              <Button onClick={toggleSidebar(anchor, true)} >{anchor}</Button>
+              <Drawer
+                anchor={anchor}
+                open={isOpen[anchor]}
+                onClose={toggleSidebar(anchor, false)}
+              >
+                {contentBox(anchor)}
+              </Drawer>
+            </div>
+            ))}
+          </div>
+      </>
     );
 }
 
